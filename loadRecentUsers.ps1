28fba58af9afc3e0faf9cff622502d009f92bbc9
jorystewart <37415682+jorystewart@users.noreply.github.com>
2018-03-15 11:52:55 -0600

    Update loadRecentUsers.ps1

--- loadRecentUsers.ps1
+++ loadRecentUsers.ps1
@@ -2,9 +2,12 @@ Write-Host "`nThis script is designed to load user state backups. You will need 
 Read-Host -Prompt "Press Enter to continue..."
 $oldComp = Read-Host -Prompt "Enter the source computer name:"
 Write-Host "`nExecuting:"
+#Replace all instances of TARGETPATH with the location where the user state is saved
 Write-Host "`nloadstate TARGETPATH /i:MigApp.xml /i:MigDocs.xml /l:TARGETPATH\loadstatelog.txt /progress:TARGETPATH\loadprogresslog.txt /ue:$oldComp\* /uel:7"
+#Replace LOADSTATE_PATH with the path to loadstate.exe
 Push-Location -Path "LOADSTATE_PATH"
 $StopWatch = [system.diagnostics.stopwatch]::startNew()
+#Replace all instances of TARGETPATH with the location where the user state is saved
 .\loadstate TARGETPATH /i:MigApp.xml /i:MigDocs.xml /l:TARGETPATH\loadstatelog.txt /progress:TARGETPATH\loadprogresslog.txt /ue:$oldComp\* /uel:7
 $StopWatch.Stop()
 Write-Host LoadState complete.
